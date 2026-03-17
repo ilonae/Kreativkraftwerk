9c356900d177fa0300188a51a31ba197cb032148
ilonae <ilonae@users.noreply.github.com>
2026-03-17 22:32:50 +0700

    Refactor: Update framework and data storage Merge branches

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './App.css';
 import NavBar from './components/NavBar';
 import Projects from './pages/Projects';
 import CollectiveNetwork from './pages/CollectiveNetwork';
