dbc6064f684de498eba52cf448922c47ffbc41e3
ilonae <ilonae@users.noreply.github.com>
2026-03-18 17:01:00 +0700

    Fix: merge update

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+export default function Button() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <button 
+        className="bg-red-500 text-white font-bold py-2 px-4 rounded hover:bg-red-700">
+        I'm a button!
+      </button>
+    </div>
+  );
+}
+
+
